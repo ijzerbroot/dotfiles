9e4972b12631a5ca038cecd225871bf92c3833ae
Frank Hoeben <Frank.Hoeben@sapienzaconsulting.com>
2021-01-10 09:31:24 +0100

    Font fix

--- doom.d/config.el
+++ doom.d/config.el
@@ -19,8 +19,8 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "CaskaydiaCove Nerd Font Mono" :size 19 :weight 'semi-light)
-      doom-variable-pitch-font (font-spec :family "CaskaydiaCove Nerd Font Mono" :size 19))
+(setq doom-font (font-spec :family "CaskaydiaCove Nerd Font" :size 18 :weight 'semi-light)
+      doom-variable-pitch-font (font-spec :family "CaskaydiaCove Nerd Font" :size 18))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
