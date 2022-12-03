c5102d11425010e8fc17d9452bd753e54f7b767c
ijzerbroot <f.hoeben@gmail.com>
2022-12-03 17:14:47 +0100

    Update init

--- init.el
+++ init.el
@@ -11,7 +11,7 @@
 ;; Set up the visible bell
 (setq visible-bell t)
 
-(set-face-attribute 'default nil :font "IBM Plex Mono" :height 170)
+(set-face-attribute 'default nil :font "Red Hat Mono" :height 160)
 
 
 ;; Make ESC quit prompts
@@ -41,19 +41,24 @@
 (straight-use-package 'org-mode)
 (straight-use-package 'consult)
 (straight-use-package 'marginalia)
-(straight-use-package 'lsp-mode)
+;;(straight-use-package 'lsp-mode)
+;; for lsp-bridge
+(straight-use-package 'posframe)
+(straight-use-package 'markdown-mode)
+(straight-use-package 'yasnippet)
+;;(straight-use-package 'lsp-bridge)
 ;;(straight-use-package 'lsp-ui)
 (straight-use-package 'avy)
 (straight-use-package 'embark)
-(straight-use-package 'corfu)
+;;(straight-use-package 'corfu)
 (straight-use-package 'prescient)
-(straight-use-package 'corfu-prescient)
-(straight-use-package 'company-prescient)
+;;(straight-use-package 'corfu-prescient)
+;;(straight-use-package 'company-prescient)
 (straight-use-package 'vertico-prescient)
 (straight-use-package 'beacon)
 (straight-use-package 'which-key)
 (straight-use-package 'rustic)
-(straight-use-package 'company-mode)
+;;(straight-use-package 'company-mode)
 (straight-use-package 'go-mode)
 (straight-use-package 'vterm)
 
@@ -77,3 +82,11 @@
  '(custom-safe-themes
    '("f64189544da6f16bab285747d04a92bd57c7e7813d8c24c30f382f087d460a33" default)))
 (load-theme 'doom-snazzy)
+
+(add-to-list 'load-path "~frank/emacs-lsp-bridge")
+
+(require 'yasnippet)
+(yas-global-mode 1)
+
+(require 'lsp-bridge)
+(global-lsp-bridge-mode)
